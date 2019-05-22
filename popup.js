ad2652c9681ceff9de3014603757b412e761d683
Zack Holmberg <zack_holmberg@outlook.com>
2019-05-21 22:14:11 -0500

    Connected with API.
    Implemented a connection to the NHL API to retrieve game information.

--- popup.js
+++ popup.js
@@ -1,16 +1,29 @@
-chrome.browserAction.onClicked.addListener(function(tab) { alert('icon clicked')});
+chrome.browserAction.onClicked.addListener(function (tab) { alert('icon clicked') });
 
-window.onload = function() {
+window.onload = function () {
   updateText();
-  function updateText(){
-    // var request = new XMLHttpRequest()
-    // request.open('GET', 'https://statsapi.web.nhl.com/api/v1/schedule', true)
-    // request.onload = function () {
-    //   // Begin accessing JSON data here
-    //   var data = JSON.parse(this.response)
-      
-    //   request.send();
-      document.getElementById("user").innerHTML = "ok good I guess";
-  //};
-}
+  function updateText() {
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://statsapi.web.nhl.com/api/v1/schedule', true)
+    request.onload = function () {
+      // Begin accessing JSON data here
+      var obj = JSON.parse(this.response)
+
+      if (request.status >= 200 && request.status < 400) {
+
+        console.log(obj.totalItems)
+        document.getElementById("user").innerHTML =
+          "Home Team: " + obj.dates[0].games[0].teams.home.team.name + " (" + obj.dates[0].games[0].teams.home.leagueRecord.wins + "/" + obj.dates[0].games[0].teams.home.leagueRecord.losses + "/" + obj.dates[0].games[0].teams.home.leagueRecord.ot + ")<br><br>" +
+          "Away Team: " + obj.dates[0].games[0].teams.away.team.name + " (" + obj.dates[0].games[0].teams.away.leagueRecord.wins + "/" + obj.dates[0].games[0].teams.away.leagueRecord.losses + "/" + obj.dates[0].games[0].teams.away.leagueRecord.ot + ")";
+
+      } else {
+        console.log('error')
+      }
+    }
+
+    request.send()
+
+
+  };
 }
